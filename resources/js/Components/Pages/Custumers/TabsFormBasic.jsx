702abcbd771029096a3ea6b868b01b977fc19bb3
Emerson Takada <ricolnegocioas@gmail.com>
2025-01-26 12:35:38 -0300

    separando components

--- resources/js/Components/Pages/Custumers/TabsFormBasic.jsx
+++ resources/js/Components/Pages/Custumers/TabsFormBasic.jsx
@@ -1,7 +1,153 @@
-import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card";
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/Components/ui/form";
+import { Input } from "@/Components/ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/Components/ui/select";
+import { useMask } from "@react-input/mask";
+import React, { useState } from "react";
 
-export default function TabsFormBasic() {
-  return (
-    <div>TabsFormBasic</div>
-  )
+export default function TabsFormBasic({ form }) {
+    const [phoneMaskedValue, setPhoneMaskedValue] = useState("");
+    const [cpfMaskedValue, setCpfMaskedValue] = useState("");
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Dados Básicos</CardTitle>
+                <CardDescription>
+                    Informações principais do cliente
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nome Completo</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input type="email" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Telefone</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        ref={useMask({
+                                            mask: "(__) _____-____",
+                                            replacement: { _: /\d/ },
+                                        })}
+                                        placeholder="(99) 99999-9999"
+                                        value={phoneMaskedValue}
+                                        onChange={(e) => {
+                                            setPhoneMaskedValue(e.target.value);
+                                            field.onChange(e);
+                                        }}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <FormField
+                        control={form.control}
+                        name="document"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>CPF</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        ref={useMask({
+                                            mask: "___.___.___.__",
+                                            replacement: { _: /\d/ },
+                                        })}
+                                        placeholder="999.999.999-99"
+                                        value={cpfMaskedValue}
+                                        onChange={(e) => {
+                                            setCpfMaskedValue(e.target.value);
+                                            field.onChange(e);
+                                        }}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="category"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Categoria</FormLabel>
+                                <Select
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Selecione uma categoria" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        <SelectItem value="regular">
+                                            Regular
+                                        </SelectItem>
+                                        <SelectItem value="vip">VIP</SelectItem>
+                                        <SelectItem value="premium">
+                                            Premium
+                                        </SelectItem>
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </CardContent>
+        </Card>
+    );
 }
--- resources/js/Pages/Customers/Form.jsx
+++ resources/js/Pages/Customers/Form.jsx
@@ -38,6 +38,7 @@ import * as z from 'zod';
 // import InputMask from 'react-input-mask';
 // import { InputMask } from 'input-mask-react'
 import { useMask } from '@react-input/mask';
+import TabsFormBasic from '@/Components/Pages/Custumers/TabsFormBasic';
 
 const formSchema = z.object({
     name: z.string().min(3, 'Nome deve ter no mínimo 3 caracteres'),
@@ -152,117 +153,8 @@ export default function CustomerForm({ customer = null }) {
                                 <Form {...form}>
                                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                                         <TabsContent value="basic">
-                                            <Card>
-                                                <CardHeader>
-                                                    <CardTitle>Dados Básicos</CardTitle>
-                                                    <CardDescription>
-                                                        Informações principais do cliente
-                                                    </CardDescription>
-                                                </CardHeader>
-                                                <CardContent className="space-y-4">
-                                                    <FormField
-                                                        control={form.control}
-                                                        name="name"
-                                                        render={({ field }) => (
-                                                            <FormItem>
-                                                                <FormLabel>Nome Completo</FormLabel>
-                                                                <FormControl>
-                                                                    <Input {...field} />
-                                                                </FormControl>
-                                                                <FormMessage />
-                                                            </FormItem>
-                                                        )}
-                                                    />
+                                        <TabsFormBasic form={form}/>
 
-                                                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                                                        <FormField
-                                                            control={form.control}
-                                                            name="email"
-                                                            render={({ field }) => (
-                                                                <FormItem>
-                                                                    <FormLabel>Email</FormLabel>
-                                                                    <FormControl>
-                                                                        <Input type="email" {...field} />
-                                                                    </FormControl>
-                                                                    <FormMessage />
-                                                                </FormItem>
-                                                            )}
-                                                        />
-
-                                                        <FormField
-                                                            control={form.control}
-                                                            name="phone"
-                                                            render={({ field }) => (
-                                                                <FormItem>
-                                                                    <FormLabel>Telefone</FormLabel>
-                                                                    <FormControl>
-                                                                             <Input
-                                                                             ref={useMask({mask: '(__) _____-____',replacement: { _: /\d/ },})}
-                                                                             placeholder='(99) 99999-9999'
-                                                                             value={phoneMaskedValue}
-                                                                                onChange={(e) =>{
-                                                                                 setPhoneMaskedValue(e.target.value)
-                                                                                   field.onChange(e)
-                                                                              }}
-                                                                            />
-                                                                    </FormControl>
-                                                                    <FormMessage />
-                                                                </FormItem>
-                                                            )}
-                                                        />
-                                                    </div>
-
-                                                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                                                        <FormField
-                                                            control={form.control}
-                                                            name="document"
-                                                            render={({ field }) => (
-                                                                <FormItem>
-                                                                    <FormLabel>CPF</FormLabel>
-                                                                    <FormControl>
-                                                                             <Input
-                                                                                ref={useMask({mask: '___.___.___.__', replacement: { _: /\d/ }, })}
-                                                                                placeholder='999.999.999-99'
-                                                                                value={cpfMaskedValue}
-                                                                                onChange={(e) => {
-                                                                                    setCpfMaskedValue(e.target.value)
-                                                                                    field.onChange(e)
-                                                                                }}
-                                                                            />
-                                                                    </FormControl>
-                                                                    <FormMessage />
-                                                                </FormItem>
-                                                            )}
-                                                        />
-
-                                                        <FormField
-                                                            control={form.control}
-                                                            name="category"
-                                                            render={({ field }) => (
-                                                                <FormItem>
-                                                                    <FormLabel>Categoria</FormLabel>
-                                                                    <Select
-                                                                        onValueChange={field.onChange}
-                                                                        defaultValue={field.value}
-                                                                    >
-                                                                        <FormControl>
-                                                                            <SelectTrigger>
-                                                                                <SelectValue placeholder="Selecione uma categoria" />
-                                                                            </SelectTrigger>
-                                                                        </FormControl>
-                                                                        <SelectContent>
-                                                                            <SelectItem value="regular">Regular</SelectItem>
-                                                                            <SelectItem value="vip">VIP</SelectItem>
-                                                                            <SelectItem value="premium">Premium</SelectItem>
-                                                                        </SelectContent>
-                                                                    </Select>
-                                                                    <FormMessage />
-                                                                </FormItem>
-                                                            )}
-                                                        />
-                                                    </div>
-                                                </CardContent>
-                                            </Card>
                                         </TabsContent>
 
                                         <TabsContent value="address">
